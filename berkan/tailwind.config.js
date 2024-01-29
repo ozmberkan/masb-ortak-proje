5f1ea8de5f456a08300768071a9837f9ad9ef3f9
berkanozm <ozmberkan@gmail.com>
2024-01-30 01:59:10 +0300

    Href Değişikliği

--- berkan/tailwind.config.js
+++ berkan/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./berkan/src/**/*.{html,js}"],
+  content: [ './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
