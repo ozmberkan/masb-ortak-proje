faac79dc80ea352545754c7a87106ff0113111a3
berkanozm <ozmberkan@gmail.com>
2024-01-30 02:10:33 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["masbortakproje/berkan/src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
